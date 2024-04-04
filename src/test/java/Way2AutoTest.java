c7978ff36bab2d8f85eeae0e462b8bec76c0adb1
zivilekut <zivilekut@gmail.com>
2024-04-04 10:34:51 +0300

    Accordion done

--- src/test/java/Way2AutoTest.java
+++ src/test/java/Way2AutoTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -33,7 +34,7 @@ public class Way2AutoTest {
         driver.close();
     }
 
-    @Test //drag and drop
+    @Test //drag and drop to a specific zone
     public void droppable(){
         driver.get("https://www.way2automation.com/way2auto_jquery/droppable.php#load_box");
         try {
@@ -64,5 +65,24 @@ public class Way2AutoTest {
        actions.moveByOffset(100, 100).build().perform();
        actions.release().build().perform();
        driver.close();
+    }
+    @Test
+    public void accordion(){
+        driver.get("https://www.way2automation.com/way2auto_jquery/accordion.php#load_box");
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.switchTo().frame(driver.findElement(By.xpath("/html/body/section/div[1]/div[1]/div[3]/div[1]/div/iframe")));
+        driver.findElement(By.id("ui-id-3")).click(); //section 2
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement resultText = driver.findElement(By.xpath("/html/body/div/div[2]/p"));
+        Assert.assertEquals(resultText.getText(), "Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In suscipit faucibus urna.");
+        driver.close();
     }
 }
