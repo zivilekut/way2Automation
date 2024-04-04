e9c8afb7bebc1c3d7db762305647f98236ab9dfe
zivilekut <zivilekut@gmail.com>
2024-04-04 09:42:15 +0300

    Droppable done

--- src/test/java/Way2AutoTest.java
+++ src/test/java/Way2AutoTest.java
@@ -1,7 +1,12 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class Way2AutoTest {
     ChromeDriver driver;
@@ -10,6 +15,23 @@ public class Way2AutoTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         driver = new ChromeDriver(options);
-        driver.get("https://www.way2automation.com/way2auto_jquery/automation-practice-site.html");
+        //driver.get("https://www.way2automation.com/way2auto_jquery/automation-practice-site.html");
+    }
+
+    @Test //drag and drop
+    public void droppable(){
+        driver.get("https://www.way2automation.com/way2auto_jquery/droppable.php#load_box");
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.switchTo().frame(driver.findElement(By.xpath("/html/body/section/div[1]/div[1]/div[3]/div[1]/div/iframe")));
+
+        WebElement draggableElement = driver.findElement(By.id("draggable"));
+        WebElement droppableElement = driver.findElement(By.id("droppable"));
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(draggableElement, droppableElement).build().perform();
+        driver.close();
     }
 }
